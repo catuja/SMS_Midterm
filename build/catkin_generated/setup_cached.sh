4cf179460833de6063e451f1eb613126518e092f
catuja <catujasmit@hotmail.com>
2022-04-20 16:25:08 +0900

    base setup of project

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/catuja/sms/SMS_Midterm/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/catuja/sms/SMS_Midterm/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/catuja/sms/SMS_Midterm/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/catuja/sms/SMS_Midterm/src:$ROS_PACKAGE_PATH"
---
+++ build/project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/catuja/sms/SMS_Midterm/src/project
+# Build directory: /home/catuja/sms/SMS_Midterm/build/project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
