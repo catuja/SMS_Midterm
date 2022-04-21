aaf3067b0fd09b90d07f9cfb145affe6bbf86b6a
catuja <catujasmit@hotmail.com>
2022-04-21 13:29:48 +0900

    added publisher

---
+++ src/project/src/publisher_node.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+
+const float SPEED = 80;
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "publisher");
+
+  ros::NodeHandle node_handle;
+
+  ros::Publisher topic_pub = node_handle.advertise<std_msgs::Float32>("speed", 10);
+
+  ros::Rate pub_rate(10);
+
+  ROS_INFO("Publishing SPEED...");
+
+  while(ros::ok())
+  {
+
+    std_msgs::Float32 msg;
+
+    msg.data = SPEED;
+
+    topic_pub.publish(msg);
+
+    pub_rate.sleep();
+  }
+
+}
